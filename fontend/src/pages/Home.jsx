20a895c8087e2a9151ee2c9af88cb80f8d66014c
Azmain Mahtab <145844907+Azmain2005@users.noreply.github.com>
2024-12-13 22:25:15 +0600

    Add files via upload

---
+++ fontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+import {Hero, Navbar, Admission,LiveCourseBenefits,Testimonials,Footer, Gallery,TopBar} from "../Components/Index"
+import "../App.css";
+function Home() {
+  return (
+   <>
+  <TopBar />
+   <Navbar/>
+   <Hero/>
+   <Admission />
+   <LiveCourseBenefits />
+   <Testimonials />
+    <Gallery />
+
+   <Footer />
+   </>
+  )
+}
+
+export default Home
